241beea400e5825399d2c692a5d984a8deb34921
root <root@ip-172-31-16-129.eu-north-1.compute.internal>
2025-08-01 14:20:16 +0000

    final code

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@
 // File: sample.java
 public class sample {  // Changed class name to match filename
     public static void main(String[] args) {
-        System.out.println("hello ahmad welcome to the jenkins with github integration");
+        System.out.println("hello ahmad welcome to the jenkins with github integration new code ");
     }
 }
