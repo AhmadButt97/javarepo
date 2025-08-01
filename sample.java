31cb5cc551797068b1ae227b8e6db1dbc6f376a6
root <root@ip-172-31-16-129.eu-north-1.compute.internal>
2025-08-01 14:05:35 +0000

    updated code

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
+
 // File: sample.java
 public class sample {  // Changed class name to match filename
     public static void main(String[] args) {
-        System.out.println("hello ahmad welcome to the jenkins");
+        System.out.println("hello ahmad welcome to the jenkins with github integration");
     }
 }
